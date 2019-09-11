7bf24b480afb1e598d8e2cd34172368883ef72c6
Jerosik <yoon161@purdue.edu>
2019-09-11 00:05:41 -0400

    Added lab2

---
+++ CS39000-WAP/lab2/scripts.js
@@ -0,0 +1,9 @@
+(function () {
+    /* Add your code here */
+}())
+
+
+let button = document.getElementById("submitButton");
+button.addEventListener("click", () => {
+    console.log("Clicked");
+})
