2110e32e5af0112428fd1be2276fcd8de8177e65
Jerosik <yoon161@purdue.edu>
2019-09-17 15:10:07 -0400

    Finished delete

--- CS39000-WAP/lab2/scripts.js
+++ CS39000-WAP/lab2/scripts.js
@@ -1,9 +1,11 @@
+// let allNoteCards = new Map();
 let allNoteCards = [];
+let selectedNoteCards = new Set([]);
 let hashID = 0;
 
 // User action when submit is pressed
-let submit = document.getElementById("submitButton");
-submit.addEventListener("click", () => {
+let submitPressed = document.getElementById("submitButton");
+submitPressed.addEventListener("click", () => {
     // Get title and text values from input field
     let title = document.getElementById("titleInput").value;
     let text = document.getElementById("inputArea").value;
@@ -20,41 +22,71 @@ submit.addEventListener("click", () => {
 })
 
 // User action when delete is pressed
-let submit = document.getElementById("submitButton");
-submit.addEventListener("click", () => {
-    // Get title and text values from input field
-    let title = document.getElementById("titleInput").value;
-    let text = document.getElementById("inputArea").value;
+let deletePressed = document.getElementById("deleteButton");
+deletePressed.addEventListener("click", () => {
+    // Go through all selected cards
+    selectedNoteCards.forEach(function(id) {
+        // Remove card from all note cards
+        
+        //allNoteCards.delete(parseInt(id, 10));
 
-    // Make sure note card at least has title
-    if (title) {
-        // Create new note card
-        createNewNoteCard(title, text);
+        // Remove HTML for card
+        document.getElementById(id).remove();
+    });
 
-        // Clear input fields
-        document.getElementById("titleInput").value = "";
-        document.getElementById("inputArea").value = "";
-    }
+    // Reset selected cards
+    selectedNoteCards = new Set([]);
 })
 
+// User action when searching
+var searchTyped = document.getElementById('searchBar');
+searchTyped.onkeyup = function() {
+    allNoteCards.forEach(function(key) {
+        
+        console.log(key);
+    })
+};
+
+// Helper function to check if card has search key
+function checkSearch() {
+
+}
+
+// Helper function to create new note card
 function createNewNoteCard(newTitle, newText) {
+    // Create HTML for note card
     let noteCardHTML = createNoteCardHTML(newTitle, newText);
+    // noteCardHTML.addEventListener("click", turnBorderOn(noteCardHTML));
     noteCardHTML.addEventListener("click", () => {
-        // noteCardHTML.className = "card border border-danger";
+        if (noteCardHTML.getAttribute("class") == "card border border-danger") {
+            noteCardHTML.className = "card";
+            selectedNoteCards.delete(noteCardHTML.getAttribute("id"));
+        } else {
+            noteCardHTML.className = "card border border-danger";
+            selectedNoteCards.add(noteCardHTML.getAttribute("id"));
+        }
     })
 
+    // Create JSON for note card
     let noteCard = {
-        title: newTitle,
-        text: newText,
         id: hashID,
+        title: newTitle,
+        text: newText
     }
 
+    // Add note card to all node card list
+    //allNoteCards.set(hashID, noteCard);
+    allNoteCards.push(noteCard);
+
+    // Append note card to HTML
     let noteCardList = document.getElementById("notecardList");
     noteCardList.appendChild(noteCardHTML);
     
+    // Increment hash ID
     hashID += 1;
 }
 
+// Helper function to create HTML of note card
 function createNoteCardHTML(newTitle, newText) {
     let cardTitle = document.createElement("h5");
     cardTitle.className = "card-title";
@@ -73,14 +105,8 @@ function createNoteCardHTML(newTitle, newText) {
 
     let card = document.createElement("div");
     card.className = "card";
+    card.setAttribute("id", hashID);
     card.appendChild(cardBody);
 
     return card;
 }
-
-// let list = document.getElementById("notecardList");
-// list.appendChild(createNewNoteCard("asdf", "asgag"));
-
-// (function () {
-//     /* Add your code here */
-// }())
