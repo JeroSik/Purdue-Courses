4dc708c5875e78caf794179508e60c586314f07c
Jerosik <yoon161@purdue.edu>
2019-09-17 18:03:48 -0400

    Changing delete

--- CS39000-WAP/lab2/scripts.js
+++ CS39000-WAP/lab2/scripts.js
@@ -24,15 +24,43 @@ submitPressed.addEventListener("click", () => {
 // User action when delete is pressed
 let deletePressed = document.getElementById("deleteButton");
 deletePressed.addEventListener("click", () => {
+    console.log(selectedNoteCards);
+
     // Go through all selected cards
     selectedNoteCards.forEach(function(id) {
         // Remove card from all note cards
-        
+        for (let i = 0; i < allNoteCards.length; i++){ 
+            if (allNoteCards[i].id == id) {
+                allNoteCards.splice(i, 1); 
+            }
+        }
         //allNoteCards.delete(parseInt(id, 10));
 
         // Remove HTML for card
-        document.getElementById(id).remove();
+        // document.getElementById(id).remove();
     });
+
+    console.log(allNoteCards);
+
+    let noteCardList = document.getElementById("notecardList");
+    while (noteCardList.firstChild) {
+        noteCardList.removeChild(noteCardList.firstChild);
+    }
+
+    allNoteCards.forEach(function(card) {
+        let temp = createNoteCardHTML(card.title, card.text)
+        temp.addEventListener("click", () => {
+            if (temp.getAttribute("class") == "card border border-danger") {
+                temp.className = "card";
+                selectedNoteCards.delete(temp.getAttribute("id"));
+            } else {
+                temp.className = "card border border-danger";
+                selectedNoteCards.add(temp.getAttribute("id"));
+            }
+        })
+
+        noteCardList.appendChild(temp);
+    })
 
     // Reset selected cards
     selectedNoteCards = new Set([]);
@@ -41,38 +69,61 @@ deletePressed.addEventListener("click", () => {
 // User action when searching
 var searchTyped = document.getElementById('searchBar');
 searchTyped.onkeyup = function() {
-    allNoteCards.forEach(function(key) {
-        
-        console.log(key);
+    console.log(this.value);
+
+    let displayList = allNoteCards.filter(checkSearch(this.value));
+
+    let noteCardList = document.getElementById("notecardList");
+    while (noteCardList.firstChild) {
+        noteCardList.removeChild(noteCardList.firstChild);
+    }
+
+    displayList.forEach(function(card) {
+        let temp = createNoteCardHTML(card.title, card.text)
+        temp.addEventListener("click", () => {
+            if (temp.getAttribute("class") == "card border border-danger") {
+                temp.className = "card";
+                selectedNoteCards.delete(temp.getAttribute("id"));
+            } else {
+                temp.className = "card border border-danger";
+                selectedNoteCards.add(temp.getAttribute("id"));
+            }
+        })
+
+        noteCardList.appendChild(temp);
     })
 };
 
 // Helper function to check if card has search key
-function checkSearch() {
-
+function checkSearch(word) {
+    return function(card) {
+        return (card.title.includes(word) || card.text.includes(word));
+    }
 }
 
 // Helper function to create new note card
 function createNewNoteCard(newTitle, newText) {
-    // Create HTML for note card
-    let noteCardHTML = createNoteCardHTML(newTitle, newText);
-    // noteCardHTML.addEventListener("click", turnBorderOn(noteCardHTML));
-    noteCardHTML.addEventListener("click", () => {
-        if (noteCardHTML.getAttribute("class") == "card border border-danger") {
-            noteCardHTML.className = "card";
-            selectedNoteCards.delete(noteCardHTML.getAttribute("id"));
-        } else {
-            noteCardHTML.className = "card border border-danger";
-            selectedNoteCards.add(noteCardHTML.getAttribute("id"));
-        }
-    })
-
     // Create JSON for note card
     let noteCard = {
         id: hashID,
         title: newTitle,
         text: newText
     }
+
+    // Create HTML for note card
+    let noteCardHTML = createNoteCardHTML(newTitle, newText);
+    noteCardHTML.addEventListener("click", () => {
+        console.log("pls");
+        if (noteCardHTML.getAttribute("class") == "card border border-danger") {
+            noteCardHTML.className = "card";
+            //selectedNoteCards.delete(noteCardHTML.getAttribute("id"));
+            selectedNoteCards.delete(noteCard);
+        } else {
+            noteCardHTML.className = "card border border-danger";
+            //selectedNoteCards.add(noteCardHTML.getAttribute("id"));
+            selectedNoteCards.add(noteCard);
+        }
+    })
 
     // Add note card to all node card list
     //allNoteCards.set(hashID, noteCard);
@@ -105,7 +156,7 @@ function createNoteCardHTML(newTitle, newText) {
 
     let card = document.createElement("div");
     card.className = "card";
-    card.setAttribute("id", hashID);
+    // card.setAttribute("id", hashID);
     card.appendChild(cardBody);
 
     return card;
