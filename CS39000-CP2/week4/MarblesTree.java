254909945a45ad83e31733b6fd7eba2536b495d4
Jerosik <yoon161@purdue.edu>
2020-02-03 19:29:22 -0500

    Start week 4 cp2

---
+++ CS39000-CP2/week4/MarblesTree.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.*;
+
+public class MarblesTree {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
---
+++ CS39000-CP2/week4/Molekule.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.*;
+
+public class Molekule {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
