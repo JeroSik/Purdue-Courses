241d21f6a1b68c110e8746bcf47a222a7d1dacb2
Jerosik <yoon161@purdue.edu>
2020-02-10 20:20:15 -0500

    start week 5

---
+++ CS39000-CP2/week7/ExactChange.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
+int main() {
+    int t, price, n, val;
+    scanf("%d", &t);
+
+    while (t-- > 0) {
+        scanf("d", &price);
+        scanf("d", &n);
+        int demon[n];
+
+        for (int i = 0; i < n; i++) {
+            scanf("d", &val);
+            demon[j] = val;
+        }
+
+        // Start dp
+        int dp[price + 1];
+        for (int i = 0; i < n; i++) {
+            scanf("d", &val);
+            demon[j] = val;
+        }
+    }
+
+    return 0;
+}
