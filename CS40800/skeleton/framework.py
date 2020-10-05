2039fd9e61819bb2e4c8989e56d7a0c91834e53b
Youngsik Yoon <@purdue.edu>
2020-10-05 15:42:53 -0400

    cs408 progress

--- CS40800/skeleton/framework.py
+++ CS40800/skeleton/framework.py
@@ -1,16 +1,38 @@
 import argparse
-import subprocess
 import os
-# These are some imports that I found useful when implementing this project, but you can use
-# other options if you prefer
+import subprocess
+import sys
 import telnetlib
 import time
-import sys
 
 # TODO: handle no -f flag
 # TODO: handle -v flag
+# Specify which line that broke i.e. expected vs response
 
-def compare_outputs(response, expected):
+def get_port(configurationFile):
+    port = None
+    with open(configurationFile, 'r') as config:
+        for line in config:
+            line = line.strip()
+            if line.startswith("PORT="):
+                port = line[6:-1]
+                break
+
+    return int(port)
+
+def compare_outputs(response, passive, expected):
+    # print('----- RESPONSE -----')
+    # print(response)
+    # print('----- PASSIVE -----')
+    # print(passive)
+    # print('----- EXPECTED -----')
+    # print(expected)
+
+    response.append("==================================================")
+    for line in passive:
+        if line != '':
+            response.append(line)
+
     print('----- RESPONSE -----')
     print(response)
     print('----- EXPECTED -----')
@@ -31,8 +53,9 @@ def run_testcase(binary_exe, input_filename, output_filename):
     output_file = open(output_filename, 'r')
     output_lines = [line.strip() for line in output_file.readlines()]
 
-    # Get configuration file
+    # Get configuration file and port
     configurationFile = "testing_environment/configurations/" + input_lines[0]
+    port = get_port(configurationFile)
 
     # Runs the BFTPD server as a subprocess. Allows program to connect to server using telnetlib framwork.
     executeLine = './' +  binary_exe + ' -c ' +  configurationFile + " -D"
@@ -41,16 +64,18 @@ def run_testcase(binary_exe, input_filename, output_filename):
 
     # Check if PORT is already taken
     if proc.poll() != None:
-        print("BFTPD failed to start, try changing your port")
+        print("BFTPD failed to start, try changing your port: {msg}".format(msg=port))
         return 2
 
-    # Create mapping of client number to telnet connection
+    # Initialize variables needed to test input
     conn_dict = dict()
+    res_list = []
+    pasv_list = []
+    pasv_mode = False
 
     # Iterate through lines of input (skip config file)
     iter_input_lines = iter(input_lines)
     next(iter_input_lines)
-    res_list = []
     for lines in iter_input_lines:
         # Parse client number and request message
         client_num, req_msg = lines.split(':')
@@ -60,13 +85,33 @@ def run_testcase(binary_exe, input_filename, output_filename):
         if client_num in conn_dict:
             client_tn = conn_dict.get(client_num)
         else:
-            client_tn = telnetlib.Telnet('localhost', 6061)
+            client_tn = telnetlib.Telnet('localhost', port)
             conn_dict[client_num] = client_tn
-            res_list.append(client_tn.read_until('\r\n').strip())
+            if not pasv_mode:
+                res_list.append(client_tn.read_until('\r\n').strip())
         
+        print(conn_dict)
         # Send request message and recieve response
-        client_tn.write(req_msg + '\r\n')
-        res_list.append(client_tn.read_until('\r\n').strip())
+        if req_msg != '':
+            client_tn.write(req_msg + '\r\n')
+            res_msg = client_tn.read_until('\r\n').strip()
+            res_list.append(res_msg)
+            if req_msg == "QUIT":
+                res_msg = client_tn.read_until('\r\n').strip()
+                res_list.append(res_msg)
+
+            code, msg = res_msg.split(" ", 1)
+            if code == "227":                   # PASV
+                pasv_mode = True
+                A_bytes = msg.split(",")
+                a1 = int(A_bytes[4])
+                a2 = int(A_bytes[5][:-1])
+                port = a1 * 256 + a2
+                continue
+
+            if pasv_mode:
+                pasv_tn = conn_dict.get('2')
+                pasv_list += pasv_tn.read_all().split('\r\n')
     
     # Close files
     input_file.close()
@@ -76,7 +121,7 @@ def run_testcase(binary_exe, input_filename, output_filename):
     os.system("ps -u $USER | grep bftpd | grep -v grep | awk '{print $1}' | xargs -r kill -9")
 
     # Return comparison between response and expected output
-    return compare_outputs(res_list, output_lines)
+    return compare_outputs(res_list, pasv_list, output_lines)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="test cases for bftpd server")
