05cd0efd7d7c101ff0144c578bed43cbfc65e72f
Youngsik Yoon <@purdue.edu>
2020-10-06 11:00:09 -0400

    cs408 all but 10

--- CS40800/skeleton/framework.py
+++ CS40800/skeleton/framework.py
@@ -20,27 +20,222 @@ def get_port(configurationFile):
 
     return int(port)
 
-def compare_outputs(response, passive, expected):
-    # print('----- RESPONSE -----')
-    # print(response)
-    # print('----- PASSIVE -----')
-    # print(passive)
-    # print('----- EXPECTED -----')
-    # print(expected)
+def get_stor(configurationFile):
+    stor = None
+    with open(configurationFile, 'r') as config:
+        for line in config:
+            line = line.strip()
+            if line.startswith("ALLOWCOMMAND_STOR="):
+                stor = line[19:-1]
+                break
 
-    response.append("==================================================")
-    for line in passive:
-        if line != '':
-            response.append(line)
+    return True if stor == 'yes' else False
 
-    print('----- RESPONSE -----')
-    print(response)
-    print('----- EXPECTED -----')
-    print(expected)
+def checkPWD(response, curr_dir, verbose):
+    expected_1 = "257 "
+    expected_1 += "\"" + os.path.abspath('./testing_environment/' + curr_dir) + "/\" "
+    expected_1 += "is the current working directory."
+
+    expected_2 = "257 "
+    expected_2 += "\"" + os.path.abspath('./testing_environment/' + curr_dir) + "\" "
+    expected_2 += "is the current working directory."
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected_1))
     
-    return 0 if response == expected else 1
+    return True if (response == expected_1 or response == expected_2) else False
 
-def run_testcase(binary_exe, input_filename, output_filename):
+def computeMKD(command):
+    dir_name = command.split(" ")[1]
+    expected = ""
+
+    if os.path.exists("./testing_environment/" + dir_name):
+        expected += "451 Error: File exists."
+    else:
+        expected += "257 "
+        expected += "\"" + dir_name + "\" "
+        expected += "has been created."
+    
+    return expected
+
+def computeRMD(command):
+    dir_name = command.split(" ")[1]
+    expected = ""
+
+    if os.path.exists("./testing_environment/" + dir_name):
+        if len(os.listdir("./testing_environment/" + dir_name)) != 0:
+            expected += "550 "
+            expected += dir_name
+            expected += ": Directory not empty"
+        else:
+            expected += "250 OK"
+    else:
+        expected += "451 Error: No such file or directory."
+    
+    return expected
+
+def checkSTAT(command, response, verbose):
+    dir_name = command.split(" ")[1]
+    expected = []
+
+    expected.append("213-Status of " + dir_name + ":")
+    if os.path.exists("./testing_environment/" + dir_name):
+        expected.append(response[1])
+        expected.append("213 End of Status.")
+    else:
+        expected.append("213-Error: No such file or directory.\n213 End of Status.")
+        expected.append("")
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+    
+    return True if response == expected else False
+
+def checkSIZE(command, response, verbose):
+    dir_name = command.split(" ")[1]
+    expected = ""
+
+    if os.path.exists("./testing_environment/" + dir_name):
+        expected = "213 " + str(os.path.getsize("./testing_environment/" + dir_name))
+    else:
+        expected = "550 Error: No such file or directory."
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+    
+    return True if response == expected else False
+
+def checkPASV(response):
+    expected = ['227', 'Entering', 'Passive', 'Mode']
+    for i in range(4):
+        if expected[i] != response[i]:
+            return False
+    
+    return True
+
+def checkNLST(response, output, pasv_mode, verbose):
+    expected = []
+
+    if pasv_mode:
+        expected.append("150 BINARY data connection established.")
+        expected.append("226 Directory list has been submitted.")
+
+        dir_list = os.listdir("./testing_environment")
+        dir_list.append("")
+        if dir_list != output:
+            return False
+    else:
+        expected.append("425-Unable to establish data connection.")
+        if response[1][0:3] != '425':
+            return False
+        expected.append(response[1])
+    
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+
+    return True if response == expected else False
+
+def checkLIST(command, response, output, pasv_mode, verbose):
+    dir_name = command.split(" ")[1] if len(command.split(" ")) != 1 else "./"
+    expected = []
+
+    if pasv_mode:
+        expected.append("150 BINARY data connection established.")
+        expected.append("226 Directory list has been submitted.")
+
+        cmd = ['ls', '-l', './testing_environment/' + dir_name]
+        ls_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        o, e = ls_proc.communicate()
+        dir_list = o.split('\n')
+        dir_list.remove(dir_list[0])
+
+        exp_set = set()
+        for item in dir_list:
+            exp_set.add(item.split(" ")[-1])
+
+        for item in output:
+            if item.split(" ")[-1] in exp_set:
+                exp_set.remove(item.split(" ")[-1])
+            else:
+                return False
+        
+        if len(exp_set) != 0:
+            return False
+    else:
+        expected.append("425-Unable to establish data connection.")
+        if response[1][0:3] != '425':
+            return False
+        expected.append(response[1])
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+
+    return True if response == expected else False
+
+def checkSTOR(command, response, pasv_mode, verbose):
+    dir_name = command.split(" ")[1]
+    expected = []
+
+    if pasv_mode:
+        if os.access('./testing_environment/' + dir_name, os.W_OK):
+            expected.append("150 BINARY data connection established.")
+            expected.append("226 File transmission successful.")
+        else:
+            expected.append("553 Error: Permission denied.")
+            expected.append("")
+    else:
+        expected.append("425-Unable to establish data connection.")
+        if response[1][0:3] != '425':
+            return False
+        expected.append(response[1])
+        os.system('rm ./testing_environment/' + dir_name)
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+
+    return True if response == expected else False
+
+def checkRETR(command, response, output, pasv_mode, verbose):
+    dir_name = command.split(" ")[1]
+    expected = []
+
+    if pasv_mode:
+        if os.path.exists("./testing_environment/" + dir_name):
+            expected.append("150 BINARY data connection established.")
+            expected.append("226 File transmission successful.")
+        else:
+            expected.append("553 Error: No such file or directory.")
+            expected.append("")
+
+    else:
+        expected.append("425-Unable to establish data connection.")
+        if response[1][0:3] != '425':
+            return False
+        expected.append(response[1])
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+
+    return True if response == expected else False
+
+def checkREST(command, response, verbose):
+    offset = command.split(" ")[1]
+    expected = "350 Restarting at offset " + offset + "."
+
+    if verbose:
+        print('Response: {input}'.format(input=response))
+        print('Expected: {input}'.format(input=expected))
+
+    return True if response == expected else False
+
+def run_testcase(binary_exe, input_filename, output_filename, verbose):
     """
     Runs a testcase given the input testcase on the FTP server
         - Returns 0 if response is expected
@@ -56,6 +251,7 @@ def run_testcase(binary_exe, input_filename, output_filename):
     # Get configuration file and port
     configurationFile = "testing_environment/configurations/" + input_lines[0]
     port = get_port(configurationFile)
+    isSTORDisabled = get_stor(configurationFile)
 
     # Runs the BFTPD server as a subprocess. Allows program to connect to server using telnetlib framwork.
     executeLine = './' +  binary_exe + ' -c ' +  configurationFile + " -D"
@@ -69,49 +265,162 @@ def run_testcase(binary_exe, input_filename, output_filename):
 
     # Initialize variables needed to test input
     conn_dict = dict()
-    res_list = []
-    pasv_list = []
+    out_index = -1
+    rest_val = 0
     pasv_mode = False
+    pasv_port = None
+    curr_dir = "./"
 
-    # Iterate through lines of input (skip config file)
+    # Set up iteration and skip config file
     iter_input_lines = iter(input_lines)
     next(iter_input_lines)
+    exit_status = 0
+
+    # Iterate through input line-by-line
     for lines in iter_input_lines:
         # Parse client number and request message
-        client_num, req_msg = lines.split(':')
+        client_num, req_command = lines.split(':')
         
         # If existing connection, get client telnet. Otherwise, create new telnet connection.
         client_tn = None
         if client_num in conn_dict:
             client_tn = conn_dict.get(client_num)
         else:
-            client_tn = telnetlib.Telnet('localhost', port)
-            conn_dict[client_num] = client_tn
             if not pasv_mode:
-                res_list.append(client_tn.read_until('\r\n').strip())
-        
-        print(conn_dict)
-        # Send request message and recieve response
-        if req_msg != '':
-            client_tn.write(req_msg + '\r\n')
-            res_msg = client_tn.read_until('\r\n').strip()
-            res_list.append(res_msg)
-            if req_msg == "QUIT":
+                client_tn = telnetlib.Telnet('localhost', port)
+                conn_dict[client_num] = client_tn
                 res_msg = client_tn.read_until('\r\n').strip()
-                res_list.append(res_msg)
+                out_index += 1
+                if res_msg != output_lines[out_index]:
+                    if verbose:
+                        print('Response: {input}'.format(input=res_msg))
+                        print('Expected: {input}'.format(input=output_lines[out_index]))
+                    exit_status = 1
+                    break
+            else:
+                client_tn = telnetlib.Telnet('localhost', pasv_port)
+                conn_dict[pasv_port] = client_tn
+        
+        # Send request message and recieve response
+        if req_command != '':
+            # Precompute expected message if necessary
+            expected = ""
+            if req_command.split(" ")[0] == 'mkd':
+                expected = computeMKD(req_command)
+            elif req_command.split(" ")[0] == 'rmd':
+                expected = computeRMD(req_command)
+            
+            # Write to telnet and get response
+            client_tn.write(req_command + '\r\n')
+            res_msg = client_tn.read_until('\r\n').strip()
 
-            code, msg = res_msg.split(" ", 1)
-            if code == "227":                   # PASV
+            # Handle different commands
+            if req_command == 'pwd':
+                if not checkPWD(res_msg, curr_dir, verbose):
+                    exit_status = 1
+                    break
+            elif req_command.split(" ")[0] == 'mkd':
+                if res_msg != expected:
+                    if verbose:
+                        print('Response: {input}'.format(input=res_msg))
+                        print('Expected: {input}'.format(input=expected))
+                    exit_status = 1
+                    break
+            elif req_command.split(" ")[0] == 'rmd':
+                if res_msg != expected:
+                    if verbose:
+                        print('Response: {input}'.format(input=res_msg))
+                        print('Expected: {input}'.format(input=expected))
+                    exit_status = 1
+                    break
+            elif req_command.split(" ")[0] == 'stat':
+                res_list = [res_msg]
+                res_list.append(client_tn.read_until('\r\n').strip())
+                res_list.append(client_tn.read_until('\r\n', 2).strip())
+                if not checkSTAT(req_command, res_list, verbose):
+                    exit_status = 1
+                    break
+            elif req_command.split(" ")[0] == 'size':
+                if not checkSIZE(req_command, res_msg, verbose):
+                    exit_status = 1
+                    break
+            elif req_command == 'pasv':
                 pasv_mode = True
-                A_bytes = msg.split(",")
+                split_msg = res_msg.split(" ")
+                if not checkPASV(split_msg):
+                    exit_status = 1
+                    break
+                A_bytes = res_msg.split(",")
                 a1 = int(A_bytes[4])
                 a2 = int(A_bytes[5][:-1])
-                port = a1 * 256 + a2
-                continue
+                pasv_port = a1 * 256 + a2
+            elif req_command == 'nlst':
+                res_list = [res_msg]
+                res_list.append(client_tn.read_until('\r\n').strip())
+                pasv_list = []
+                if pasv_mode:
+                    pasv_tn = conn_dict.get(pasv_port)
+                    pasv_list += pasv_tn.read_all().split('\r\n')
+                if not checkNLST(res_list, pasv_list, pasv_mode, verbose):
+                    exit_status = 1
+                    break
+                pasv_mode = False
+            elif req_command.split(" ")[0] == 'list':
+                res_list = [res_msg]
+                res_list.append(client_tn.read_until('\r\n').strip())
+                pasv_list = []
+                if pasv_mode:
+                    pasv_tn = conn_dict.get(pasv_port)
+                    pasv_list += pasv_tn.read_all().split('\r\n')
+                if not checkLIST(req_command, res_list, pasv_list, pasv_mode, verbose):
+                    exit_status = 1
+                    break
+                pasv_mode = False
+            elif isSTORDisabled and req_command.split(" ")[0] == 'stor':
+                if pasv_mode:
+                    pasv_tn = conn_dict.get(pasv_port)
+                    pasv_tn.write("0123456789\r\n")
+                    pasv_tn.close()
+                res_list = [res_msg]
+                res_list.append(client_tn.read_until('\r\n', 2).strip())
+                if not checkSTOR(req_command, res_list, pasv_mode, verbose):
+                    exit_status = 1
+                    break
+                pasv_mode = False
+            elif req_command.split(" ")[0] == 'retr':
+                res_list = [res_msg]
+                res_list.append(client_tn.read_until('\r\n', 2).strip())
+                pasv_list = []
+                if pasv_mode and res_msg[0:3] != '553':
+                    pasv_tn = conn_dict.get(pasv_port)
+                    pasv_list += pasv_tn.read_all().split('\r\n')
+                if not checkRETR(req_command, res_list, pasv_list, pasv_mode, verbose):
+                    exit_status = 1
+                    os.system('rm ./testing_environment/' + req_command.split(" ")[1])
+                    break
+                if os.path.exists("./testing_environment/" + req_command.split(" ")[1]) and req_command.split(" ")[1] != 'password_list':
+                    os.system('rm ./testing_environment/' + req_command.split(" ")[1])
+                pasv_mode = False
+            elif req_command.split(" ")[0] == 'rest':
+                rest_val = int(req_command.split(" ")[1])
+                if not checkREST(req_command, res_msg, verbose):
+                    exit_status = 1
+                    break
+            else:
+                out_index += 1
+                if res_msg != output_lines[out_index]:
+                    if verbose:
+                        print('Command: {input}'.format(input=lines))
+                        print('Response: {input}'.format(input=res_msg))
+                        print('Expected: {input}'.format(input=output_lines[out_index]))
+                    exit_status = 1
+                    break
 
-            if pasv_mode:
-                pasv_tn = conn_dict.get('2')
-                pasv_list += pasv_tn.read_all().split('\r\n')
+                if req_command.split(" ")[0] == 'cwd':
+                    curr_dir += req_command.split(" ")[1]
+                elif req_command.split(" ")[0] == 'cdup':
+                    curr_dir += '../'
+        
     
     # Close files
     input_file.close()
@@ -121,7 +430,7 @@ def run_testcase(binary_exe, input_filename, output_filename):
     os.system("ps -u $USER | grep bftpd | grep -v grep | awk '{print $1}' | xargs -r kill -9")
 
     # Return comparison between response and expected output
-    return compare_outputs(res_list, pasv_list, output_lines)
+    return exit_status
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="test cases for bftpd server")
@@ -135,19 +444,26 @@ if __name__ == '__main__':
         print("Usage: python framework.py -p binary [-c conf] [-f file] [-v verbose]")
         sys.exit(2)
 
+    isVerbose = False
+    if args.verbose != None:
+        isVerbose = True
+
     # Run testcases on either given file or all files
     exit_status = -1
     if args.file == None:
         # Iterate through all test inputs
-        for filename in os.listdir('./testing_environment/testcases'):
-            if filename.startswith('test_input'):
-                print(filename)
+        for file_num in range(1,35):
+            print('Running test on test_input_{num}'.format(num=file_num))
+            filename = './testing_environment/testcases/test_input_' + str(file_num) + '.txt'
+            exit_status = run_testcase(args.binary, filename, filename.replace('input', 'output'), isVerbose)
+            if exit_status == 1:
+                break
     else:
         print('Running test on {input}'.format(input=args.file))
-        exit_status = run_testcase(args.binary, args.file, args.file.replace('input', 'output'))
+        exit_status = run_testcase(args.binary, args.file, args.file.replace('input', 'output'), isVerbose)
     
-    if exit_status == 0:
-        print('\033[92m' + 'PASS' + '\033[0m')
-    elif exit_status == 1:
-        print('\033[91m' + 'FAIL' + '\033[0m')
+    # if exit_status == 0:
+    #     print('\033[92m' + 'PASS' + '\033[0m')
+    # elif exit_status == 1:
+    #     print('\033[91m' + 'FAIL' + '\033[0m')
     sys.exit(exit_status)
--- CS40800/skeleton/grade.sh
+++ CS40800/skeleton/grade.sh
@@ -6,8 +6,11 @@ echo "TEST FRAMEWORK"
 echo "==================== bftpd_{clean} ===================="
 python framework.py -p ./bin/bftpd_clean -f ./testing_environment/testcases/test_input_$test_num.txt
 
-for i in {1..10}
-do
-    echo "==================== bftpd_{$i} ===================="
-    python framework.py -p ./bin/bftpd_$i -f ./testing_environment/testcases/test_input_$test_num.txt
-done
+# for i in {1..10}
+# do
+#     echo "==================== bftpd_{$i} ===================="
+#     python framework.py -p ./bin/bftpd_$i -f ./testing_environment/testcases/test_input_$test_num.txt
+# done
+
+echo "==================== bftpd_10 ===================="
+python framework.py -p ./bin/bftpd_10 -f ./testing_environment/testcases/test_input_$test_num.txt
