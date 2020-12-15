46069a27b61bdfa7bb730877af5da063e442458f
JeroSik <yoon161@purdue.edu>
2020-12-14 22:53:02 -0700

    yes

--- CS35200/tests/sandbox/test.java
+++ CS35200/tests/sandbox/test.java
@@ -1,11 +1,18 @@
-class test {
+class Test{
     public static void main(String[] args) {
-        a a = new a();
-        int b = a.a;
-        System.out.println(b);
+		int a = 2;
+        int b = 3;
+        while(a < 10){
+            System.out.print("a: ");
+            System.out.println(a);
+            while(b <= 7){
+                System.out.print("b: ");
+                System.out.println(b);
+                a = a - 1;
+                b = b + 1;
+            }
+            a = a + 1;
+        }
+        System.out.println("One final print");
     }
-}
-
-class a {
-    int a;
 }
